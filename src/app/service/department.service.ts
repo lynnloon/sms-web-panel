fc0398678e4298fe54e0771c6f7334b2f08ceadf
Thu Soe San <thusoesan69@gmail.com>
2024-06-17 16:19:43 +0630

    department UI

---
+++ src/app/service/department.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CommonService } from '../util/common.service';
+import { HttpClient } from '@angular/common/http';
+import { Department } from '../model/department';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+
+  constructor(
+    private httpClient: HttpClient,
+    private commonService: CommonService
+  ) { }
+
+  getAllDepartmentList() {
+    return this.httpClient.get(this.commonService.apiRoute + "/department/getAll")
+  }
+
+  create(department: Department) {
+    return this.httpClient.post(this.commonService.apiRoute + "/department/save", department);
+  }
+
+  getById(id: any) {
+   return this.httpClient.get(this.commonService.apiRoute+"/department/getById?id="+id);
+  }
+
+  delete(id: any) {
+  return this.httpClient.delete(this.commonService.apiRoute+"/department/delete?id="+id);
+  }
+
+}
