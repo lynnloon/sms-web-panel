4be8830b3ce2830396b1b739035251a675838081
Khun Maung Ngwee <khunmgngwe@ucstgi.edu.mm>
2024-07-01 11:22:04 +0630

    side bar edit .ts

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
-  role : string  | null = null;
+  role?: string ;
 
   constructor(){
-    this.role = localStorage.getItem('userrole');
+    
+  }
+  ngOnInit(){
+   this.role= localStorage.getItem('userrole') as string;
   }
 }
