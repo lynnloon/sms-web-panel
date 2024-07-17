a1e7ec50da9e0aed0ffe78152d812cb1af84f90f
DESKTOP-KURPU16\Dell <chawsuhh562@gmail.com>
2024-07-17 21:36:48 +0630

    timetable

--- src/app/model/filter-dto.ts
+++ src/app/model/filter-dto.ts
@@ -3,4 +3,8 @@ export class FilterDTO {
     batchId?: number;
     semesterId?: number;
     major?: string;
+    section?:number;
+    studentId?: number;
+    academicYear?: number;
+
 }
---
+++ src/app/service/timetable.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, OnInit } from '@angular/core';
+import { CommonService } from '../util/common.service';
+import { Timetable } from '../model/timetable';
+import { HttpClient } from '@angular/common/http';
+import { FilterDTO } from '../model/filter-dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimetableService implements OnInit {
+
+  constructor(
+    private commonService:CommonService,
+    private httpClient:HttpClient
+
+  ) { }
+  ngOnInit(): void {
+  
+  }
+  save(timetable:Timetable[]){
+    return this.httpClient.post(this.commonService.apiRoute+"/timetable/save",timetable);
+  }
+  getTimetableList(filter:FilterDTO)
+  {
+    return this.httpClient.post(this.commonService.apiRoute+"/timetable/retrieveTimetable",filter);
+  }
+getSection(filter:FilterDTO)
+{
+  return this.httpClient.post(this.commonService.apiRoute+"/timetable/getSection",filter);
+  
+} 
+}
